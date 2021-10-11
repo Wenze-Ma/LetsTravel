4bef2eedbee16229199310824368d3126017093c
Wenze Ma <maw1@rose-hulman.edu>
2021-10-11 13:37:07 -0400

    hello world

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { DatePicker, message } from 'antd';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+        <header className="App-header">
+            <p>Hello World!</p>
+        </header>
     </div>
   );
 }
