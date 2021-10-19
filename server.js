2eb224f35fff09e1af5dd3fc6eb0f287629e2f8d
Wenze Ma <maw1@rose-hulman.edu>
2021-10-19 17:56:51 -0400

    Update server.js

--- server.js
+++ server.js
@@ -37,13 +37,10 @@ app.use(bodyParser.json());
 app.use("/users", usersRouter);
 
 
-if(process.env.NODE_ENV === 'production') {
-    // set static folder
-    app.use(express.static('client/build'));
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'public/index.html'));
-    // });
-}
+app.use(express.static(path.join(__dirname, 'client/build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
 app.listen(port, function () {
     console.log("Running on " + port);
