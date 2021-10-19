13cef2023588ce2e35e729a5b11290d14985a03a
Wenze Ma <maw1@rose-hulman.edu>
2021-10-19 17:28:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 // const config = require("./dbConfig");
 
 const usersRouter = require("./routes/users");
