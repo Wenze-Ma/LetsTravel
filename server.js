32a77594c2b4fd7924b8f2dceb89ce0acdec8a69
Wenze Ma <maw1@rose-hulman.edu>
2021-10-19 17:00:39 -0400

    restructure the app

--- server.js
+++ server.js
@@ -40,9 +40,9 @@ app.use("/users", usersRouter);
 if(process.env.NODE_ENV === 'production') {
     // set static folder
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'public/index.html'));
-    });
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(__dirname, 'client', 'build', 'public/index.html'));
+    // });
 }
 
 app.listen(port, function () {
